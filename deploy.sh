55430806b61222e39ee5dee8215202c8cc4cb828
Gxliu <74167178+kpol-lgx@users.noreply.github.com>
2024-04-15 19:10:06 +0800

    Add deploy.sh
    install hexo and hexo init hexo

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt -y install git curl
+
+# install nvs for manage node.js
+export NVS_HOME="$HOME/.nvs"
+if [ -d "$NVS_HOME" ]; then
+    echo "destination path ${NVS_HOME} already exists and is not an empty directory"
+else
+    git clone https://github.com/jasongin/nvs "$NVS_HOME"
+fi
+. "$NVS_HOME/nvs.sh" install
+
+# add node.js and use it
+nvs add lts && nvs link lts
+
+# install hexo
+npm install -g hexo-cli
+
+# hexo deploy
+cd ~
+mkdir -v hexo
+hexo init hexo
+cd hexo
+    npm install
+cd ..
+
+# add node_modules to path
+# then replace 'npx hexo' with 'hexo'
+if grep -qF 'PATH="$PATH:./node_modules/.bin"' ~/.profile; then
+  echo "has exist! node_modules to path in ~/.profile"
+else
+  echo 'PATH="$PATH:./node_modules/.bin"' >> ~/.profile
+  echo "has append! node_modules to path in ~/.profile"
+fi
